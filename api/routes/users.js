1ccbe9e49a91beefd4a310f6bb90709c6e5ff3bf
Muhammad Kashan <112327092+Kashan724@users.noreply.github.com>
2024-05-11 20:14:34 +0500

    added files

---
+++ api/routes/users.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/usercontroller.js";
+
+import { verifyToken,verifyUser,verifyAdmin} from "../utils/verifyToken.js";
+
+
+const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+       res.send("hello user, you are logged in")
+     })
+    
+     router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+          res.send("hello user, you are logged in and you can delete your account")
+         })
+
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
+
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+//GET
+router.get("/:id", verifyUser, getUser);
+
+//GET ALL
+router.get("/", verifyAdmin,getUsers);
+
+
+export default router;
